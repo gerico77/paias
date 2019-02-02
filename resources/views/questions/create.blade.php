b75915adc02e618f72ca446d69a1681054910935
Gerico <gericosv@gmail.com>
2019-02-03 00:28:30 +0800

    question view design improvements

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -1,148 +1,126 @@
 @extends('layouts.app')
 
 @section('content')
-    <br>
-    <div class="container">
+    <div class="container-fluid">
     <h3 class="page-title">Questions</h3>
     {!! Form::open(['method' => 'POST', 'route' => ['questions.store']]) !!}
 
-    <div class="panel panel-default">
-        <div class="panel-heading">
+    <div class="card mb-3">
+        <div class="card-header">
+            <i class="fas fa-plus"></i>
             Create
         </div>
 
-        <div class="panel-body">
-            <div class="row">
-                <div class="col-xs-12 form-group">
-                    {!! Form::label('subject_id', 'Subject*', ['class' => 'control-label']) !!}
-                    {!! Form::select('subject_id', $subjects, old('subject_id'), ['class' => 'form-control']) !!}
-                    <p class="help-block"></p>
-                    @if($errors->has('subject_id'))
-                        <p class="help-block">
-                            {{ $errors->first('subject_id') }}
-                        </p>
-                    @endif
-                </div>
+        <div class="card-body">
+            <div class="form-group">
+                {!! Form::label('subject_id', 'Subject*') !!}
+                {!! Form::select('subject_id', $subjects, old('subject_id'), ['class' => 'form-control']) !!}
+                <small class="form-text text-muted"></small>
+                @if($errors->has('subject_id'))
+                    <small class="form-text text-muted">
+                        {{ $errors->first('subject_id') }}
+                    </small>
+                @endif
             </div>
-            <div class="row">
-                <div class="col-xs-12 form-group">
-                    {!! Form::label('question_text', 'Question text*', ['class' => 'control-label']) !!}
-                    {!! Form::textarea('question_text', old('question_text'), ['class' => 'form-control ', 'placeholder' => '']) !!}
-                    <p class="help-block"></p>
-                    @if($errors->has('question_text'))
-                        <p class="help-block">
-                            {{ $errors->first('question_text') }}
-                        </p>
-                    @endif
-                </div>
+            <div class="form-group">
+                {!! Form::label('question_text', 'Question text*') !!}
+                {!! Form::textarea('question_text', old('question_text'), ['class' => 'form-control', 'placeholder' => '']) !!}
+                <small class="form-text text-muted"></small>
+                @if($errors->has('question_text'))
+                    <small class="form-text text-muted">
+                        {{ $errors->first('question_text') }}
+                    </small>
+                @endif
             </div>
-            <div class="row">
-                <div class="col-xs-12 form-group">
-                    {!! Form::label('option1', 'Option #1', ['class' => 'control-label']) !!}
-                    {!! Form::text('option1', old('option1'), ['class' => 'form-control ', 'placeholder' => '']) !!}
-                    <p class="help-block"></p>
-                    @if($errors->has('option1'))
-                        <p class="help-block">
-                            {{ $errors->first('option1') }}
-                        </p>
-                    @endif
-                </div>
+            <div class="form-group">
+                {!! Form::label('option1', 'Option #1', ['class' => 'control-label']) !!}
+                {!! Form::text('option1', old('option1'), ['class' => 'form-control ', 'placeholder' => '']) !!}
+                <small class="form-text text-muted"></small>
+                @if($errors->has('option1'))
+                    <small class="form-text text-muted">
+                        {{ $errors->first('option1') }}
+                    </small>
+                @endif
             </div>
-            <div class="row">
-                <div class="col-xs-12 form-group">
-                    {!! Form::label('option2', 'Option #2', ['class' => 'control-label']) !!}
-                    {!! Form::text('option2', old('option2'), ['class' => 'form-control ', 'placeholder' => '']) !!}
-                    <p class="help-block"></p>
-                    @if($errors->has('option2'))
-                        <p class="help-block">
-                            {{ $errors->first('option2') }}
-                        </p>
-                    @endif
-                </div>
+            <div class="form-group">
+                {!! Form::label('option2', 'Option #2', ['class' => 'control-label']) !!}
+                {!! Form::text('option2', old('option2'), ['class' => 'form-control ', 'placeholder' => '']) !!}
+                <small class="form-text text-muted"></small>
+                @if($errors->has('option2'))
+                    <small class="form-text text-muted">
+                        {{ $errors->first('option2') }}
+                    </small>
+                @endif
             </div>
-            <div class="row">
-                <div class="col-xs-12 form-group">
-                    {!! Form::label('option3', 'Option #3', ['class' => 'control-label']) !!}
-                    {!! Form::text('option3', old('option3'), ['class' => 'form-control ', 'placeholder' => '']) !!}
-                    <p class="help-block"></p>
-                    @if($errors->has('option3'))
-                        <p class="help-block">
-                            {{ $errors->first('option3') }}
-                        </p>
-                    @endif
-                </div>
+            <div class="form-group">
+                {!! Form::label('option3', 'Option #3', ['class' => 'control-label']) !!}
+                {!! Form::text('option3', old('option3'), ['class' => 'form-control ', 'placeholder' => '']) !!}
+                <small class="form-text text-muted"></small>
+                @if($errors->has('option3'))
+                    <small class="form-text text-muted">
+                        {{ $errors->first('option3') }}
+                    </small>
+                @endif
             </div>
-            <div class="row">
-                <div class="col-xs-12 form-group">
-                    {!! Form::label('option4', 'Option #4', ['class' => 'control-label']) !!}
-                    {!! Form::text('option4', old('option4'), ['class' => 'form-control ', 'placeholder' => '']) !!}
-                    <p class="help-block"></p>
-                    @if($errors->has('option4'))
-                        <p class="help-block">
-                            {{ $errors->first('option4') }}
-                        </p>
-                    @endif
-                </div>
+            <div class="form-group">
+                {!! Form::label('option4', 'Option #4', ['class' => 'control-label']) !!}
+                {!! Form::text('option4', old('option4'), ['class' => 'form-control ', 'placeholder' => '']) !!}
+                <small class="form-text text-muted"></small>
+                @if($errors->has('option4'))
+                    <small class="form-text text-muted">
+                        {{ $errors->first('option4') }}
+                    </small>
+                @endif
             </div>
-            <div class="row">
-                <div class="col-xs-12 form-group">
-                    {!! Form::label('option5', 'Option #5', ['class' => 'control-label']) !!}
-                    {!! Form::text('option5', old('option5'), ['class' => 'form-control ', 'placeholder' => '']) !!}
-                    <p class="help-block"></p>
-                    @if($errors->has('option5'))
-                        <p class="help-block">
-                            {{ $errors->first('option5') }}
-                        </p>
-                    @endif
-                </div>
+            <div class="form-group">
+                {!! Form::label('option5', 'Option #5', ['class' => 'control-label']) !!}
+                {!! Form::text('option5', old('option5'), ['class' => 'form-control ', 'placeholder' => '']) !!}
+                <small class="form-text text-muted"></small>
+                @if($errors->has('option5'))
+                    <small class="form-text text-muted">
+                        {{ $errors->first('option5') }}
+                    </small>
+                @endif
             </div>
-            <div class="row">
-                <div class="col-xs-12 form-group">
-                    {!! Form::label('correct', 'Correct', ['class' => 'control-label']) !!}
-                    {!! Form::select('correct', $correct_options, old('correct'), ['class' => 'form-control']) !!}
-                    <p class="help-block"></p>
-                    @if($errors->has('correct'))
-                        <p class="help-block">
-                            {{ $errors->first('correct') }}
-                        </p>
+            <div class="form-group">
+                {!! Form::label('correct', 'Correct', ['class' => 'control-label']) !!}
+                {!! Form::select('correct', $correct_options, old('correct'), ['class' => 'form-control']) !!}
+                <small class="form-text text-muted"></small>
+                @if($errors->has('correct'))
+                    <small class="form-text text-muted">
+                        {{ $errors->first('correct') }}
+                    </small>
                     @endif
-                </div>
             </div>
-            <div class="row">
-                <div class="col-xs-12 form-group">
-                    {!! Form::label('code_snippet', 'Code snippet', ['class' => 'control-label']) !!}
-                    {!! Form::textarea('code_snippet', old('code_snippet'), ['class' => 'form-control ', 'placeholder' => '']) !!}
-                    <p class="help-block"></p>
-                    @if($errors->has('code_snippet'))
-                        <p class="help-block">
-                            {{ $errors->first('code_snippet') }}
-                        </p>
-                    @endif
-                </div>
+            <div class="form-group">
+                {!! Form::label('code_snippet', 'Code snippet', ['class' => 'control-label']) !!}
+                {!! Form::textarea('code_snippet', old('code_snippet'), ['class' => 'form-control ', 'placeholder' => '']) !!}
+                <small class="form-text text-muted"></small>
+                @if($errors->has('code_snippet'))
+                    <small class="form-text text-muted">
+                        {{ $errors->first('code_snippet') }}
+                    </small>
+                @endif
             </div>
-            <div class="row">
-                <div class="col-xs-12 form-group">
-                    {!! Form::label('answer_explanation', 'Answer explanation*', ['class' => 'control-label']) !!}
-                    {!! Form::textarea('answer_explanation', old('answer_explanation'), ['class' => 'form-control ', 'placeholder' => '']) !!}
-                    <p class="help-block"></p>
-                    @if($errors->has('answer_explanation'))
-                        <p class="help-block">
-                            {{ $errors->first('answer_explanation') }}
-                        </p>
-                    @endif
-                </div>
+            <div class="form-group">
+                {!! Form::label('answer_explanation', 'Answer explanation*', ['class' => 'control-label']) !!}
+                {!! Form::textarea('answer_explanation', old('answer_explanation'), ['class' => 'form-control ', 'placeholder' => '']) !!}
+                <small class="form-text text-muted"></small>
+                @if($errors->has('answer_explanation'))
+                    <small class="form-text text-muted">
+                        {{ $errors->first('answer_explanation') }}
+                    </small>
+                @endif
             </div>
-            <div class="row">
-                <div class="col-xs-12 form-group">
-                    {!! Form::label('more_info_link', 'More info link', ['class' => 'control-label']) !!}
-                    {!! Form::text('more_info_link', old('more_info_link'), ['class' => 'form-control', 'placeholder' => '']) !!}
-                    <p class="help-block"></p>
-                    @if($errors->has('more_info_link'))
-                        <p class="help-block">
-                            {{ $errors->first('more_info_link') }}
-                        </p>
-                    @endif
-                </div>
+            <div class="form-group">
+                {!! Form::label('more_info_link', 'More info link', ['class' => 'control-label']) !!}
+                {!! Form::text('more_info_link', old('more_info_link'), ['class' => 'form-control', 'placeholder' => '']) !!}
+                <small class="form-text text-muted"></small>
+                @if($errors->has('more_info_link'))
+                    <small class="form-text text-muted">
+                        {{ $errors->first('more_info_link') }}
+                    </small>
+                @endif
             </div>
 
         </div>
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -1,59 +1,74 @@
 @extends('layouts.app')
 
 @section('content')
-    <br>
-    <div class="container">
-   <h3 class="page-title">Questions</h3>
-
-    <p>
-        <a href="{{ route('questions.create') }}" class="btn btn-primary">Add New</a>
-    </p>
-
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            List
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+            </div>
         </div>
-
-        <div class="panel-body">
-            <table class="table table-bordered table-striped {{ count($questions) > 0 ? 'datatable' : '' }} dt-select">
-                <thead>
-                    <tr>
-                        <th style="text-align:center;"><input type="checkbox" id="select-all" /></th>
-                        <th>Subject</th>
-                        <th>Question</th>
-                        <th>&nbsp;</th>
-                    </tr>
-                </thead>
-                
-                <tbody>
-                    @if (count($questions) > 0)
-                        @foreach ($questions as $question)
-                            <tr data-entry-id="{{ $question->id }}">
-                                <td></td>
-                                <td>{{ $question->subject->title or '' }}</td>
-                                <td>{!! $question->question_text !!}</td>
-                                <td>
-                                    <a href="{{ route('questions.show',[$question->id]) }}" class="btn btn-xs btn-success">View</a>
-                                    <a href="{{ route('questions.edit',[$question->id]) }}" class="btn btn-xs btn-info">Edit</a>
-                                    {!! Form::open(array(
-                                        'style' => 'display: inline-block;',
-                                        'method' => 'DELETE',
-                                        'onsubmit' => "return confirm('".trans("Are you sure?")."');",
-                                        'route' => ['questions.destroy', $question->id])) !!}
-                                    {!! Form::submit(trans('Delete'), array('class' => 'btn btn-xs btn-danger')) !!}
-                                    {!! Form::close() !!}
-                                </td>
-                            </tr>
-                        @endforeach
-                    @else
-                        <tr>
-                            <td colspan="7">@lang('No entries in table')</td>
-                        </tr>
-                    @endif
-                </tbody>
-            </table>
+    
+        <h3 class="page-title">Questions</h3>
+    
+        <p>
+            <a href="{{ route('questions.create') }}" class="btn btn-success btn-sm">
+                <i class="fas fa-plus"></i>
+                Add new
+            </a>
+        </p>
+    
+        <div class="card mb-3">
+            <div class="card-header">
+                <i class="fas fa-table"></i>
+                List
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-hover" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <th>Subject</th>
+                            <th>Question Text</th>
+                            <th style="width:30%">&nbsp;</th>
+                        </thead>
+                        
+                        <tbody>
+                            @if (count($questions) > 0)
+                                @foreach ($questions as $question)
+                                    <tr data-entry-id="{{ $question->id }}">
+                                        <td>{{ $question->subject->title}}</td>
+                                        <td>{!! $question->question_text !!}</td>
+                                        <td>
+                                            <a href="{{ route('questions.show',[$question->id]) }}" class="btn btn-sm btn-primary">
+                                                <i class="fas fa-eye"></i>
+                                                View
+                                            </a>
+                                            <a href="{{ route('questions.edit',[$question->id]) }}" class="btn btn-sm btn-info">
+                                                <i class="fas fa-edit"></i>
+                                                Edit
+                                            </a>
+                                            {!! Form::open(array(
+                                                'style' => 'display: inline-block;',
+                                                'method' => 'DELETE',
+                                                'onsubmit' => "return confirm('Are you sure?');",
+                                                'route' => ['questions.destroy', $question->id])) !!}
+                                            {!! Form::button('<i class="fas fa-trash-alt"></i> Delete', array('type' => 'submit', 'class' => 'btn btn-sm btn-danger')) !!}
+                                            {!! Form::close() !!}
+                                        </td>
+                                    </tr>
+                                    
+                                @endforeach
+                            @else
+                                
+                            @endif
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-    </div>
     </div>
 @stop
 
