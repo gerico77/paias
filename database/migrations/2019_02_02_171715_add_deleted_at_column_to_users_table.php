65fcea241e47ba01cfcc943feadae43455849614
Gerico <gericosv@gmail.com>
2019-02-03 02:41:42 +0800

    Migrations - Delete unused

--- database/migrations/2019_02_02_171715_add_deleted_at_column_to_users_table.php
+++ database/migrations/2019_02_02_171715_add_deleted_at_column_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRolesTable extends Migration
+class AddDeletedAtColumnToUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,7 @@ class CreateRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('roles', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('title');
-
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
             $table->softDeletes();
 
             $table->index(['deleted_at']);
@@ -31,6 +27,8 @@ class CreateRolesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('roles');
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
     }
 }
