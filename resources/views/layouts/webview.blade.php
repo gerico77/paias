b8b690ca02bc44a84f2bb1c9a2458efe5822cf63
Gerico <gericosv@gmail.com>
2019-02-16 23:36:09 +0800

    Webview implementation

---
+++ resources/views/layouts/webview.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    @include('partials.head')
+</head>
+<body id="page-top">
+
+    <div id="wrapper">
+
+        <div id="content-wrapper">
+            <div class="container-fluid">
+                @if (Session::has('message'))
+                    <div class="alert alert-info" role="alert">
+                        {{ Session::get('message') }}
+                    </div>
+                @endif
+                @if ($errors->count() > 0)
+                    @foreach($errors->all() as $error)
+                        <div class="alert alert-danger" role="alert">
+                            {{ $error }}
+                        </div>
+                    @endforeach
+                @endif
+                @yield('content')
+            </div>
+        </div>
+    </div>
+    <div class="scroll-to-top" style="display: none;">
+        <i class="fa fa-arrow-up"></i>
+    </div>
+
+    @include('partials.javascripts')
+</body>
+</html>
