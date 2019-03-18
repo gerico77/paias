463aa4f5d7435ad4adeccfc25da9e9ac62c61d29
Mechelou <paiasofficialacct@gmail.com>
2019-03-19 02:17:42 +0800

    Import and Export

---
+++ app/Imports/UsersImport.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Imports;
+
+use App\User;
+use Maatwebsite\Excel\Concerns\ToModel;
+
+class UsersImport implements ToModel
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        return new User([
+            'username' => $row[1],
+            'fname' => $row[2],
+            'lname' => $row[3],
+            'email' => $row[4],
+            'password' => '',
+        ]);
+    }
+}
