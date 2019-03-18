69f340658a6088bc61cfa9f8478d0e34c992503f
Mechelou <paiasofficialacct@gmail.com>
2019-03-19 02:21:37 +0800

    True or False

---
+++ resources/views/questions/truefalse/create.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+    <h3 class="page-title">Questions</h3>
+    {!! Form::open(['method' => 'POST', 'route' => ['questions.store']]) !!}
+    {!! Form::hidden('qtype', 'truefalse') !!}
+    <div class="card mb-3">
+        <div class="card-header">
+            <i class="fas fa-plus"></i>
+            Create
+        </div>
+
+        <div class="card-body">
+            <div class="form-group">
+                {!! Form::label('subject_id', 'Subject*') !!}
+                {!! Form::select('subject_id', $subjects, old('subject_id'), ['class' => 'form-control']) !!}
+                <small class="form-text text-muted"></small>
+                @if($errors->has('subject_id'))
+                    <small class="form-text text-muted">
+                        {{ $errors->first('subject_id') }}
+                    </small>
+                @endif
+            </div>
+            <div class="form-group">
+                {!! Form::label('question_text', 'Question text*') !!}
+                {!! Form::textarea('question_text', old('question_text'), ['class' => 'form-control', 'placeholder' => '']) !!}
+                <small class="form-text text-muted"></small>
+                @if($errors->has('question_text'))
+                    <small class="form-text text-muted">
+                        {{ $errors->first('question_text') }}
+                    </small>
+                @endif
+            </div>
+            {!! Form::hidden('option1', 'True') !!}
+            {!! Form::hidden('option2', 'False') !!}
+            <div class="form-group">
+                {!! Form::label('correct', 'Correct*', ['class' => 'control-label']) !!}
+                {!! Form::select('correct', $correct_options, old('correct'), ['class' => 'form-control']) !!}
+                <small class="form-text text-muted"></small>
+                @if($errors->has('correct'))
+                    <small class="form-text text-muted">
+                        {{ $errors->first('correct') }}
+                    </small>
+                    @endif
+            </div>
+            <div class="form-group">
+                {!! Form::label('answer_explanation', 'Answer explanation*', ['class' => 'control-label']) !!}
+                {!! Form::textarea('answer_explanation', old('answer_explanation'), ['class' => 'form-control ', 'placeholder' => '']) !!}
+                <small class="form-text text-muted"></small>
+                @if($errors->has('answer_explanation'))
+                    <small class="form-text text-muted">
+                        {{ $errors->first('answer_explanation') }}
+                    </small>
+                @endif
+            </div>
+
+            {!! Form::submit('Save', ['class' => 'btn btn-success']) !!}
+        </div>
+    </div>
+    {!! Form::close() !!}
+    </div>
+@stop
+
